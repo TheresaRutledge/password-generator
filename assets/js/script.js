847c73238f4d305111e87cee7a6b95455f8ddf21
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-02 21:10:34 -0700

    length function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
 // Assignment code here
 
 
+//function to prompt user for password length between 8-128
+const getPasswordLength = () => {
+  let passwordLength = prompt('Choose password length between 8 and 128 characters.');
+  passwordLength = parseInt(passwordLength);
+  if (!passwordLength){
+    alert('Length must be a number');
+    getPasswordLength();
+  } else if(passwordLength<8 || passwordLength > 128){
+    alert('Password must be between 8 and 128 characters.');
+    getPasswordLength();
+  }
+}
+
+
+//generates password based on user prompt inputs
+const generatePassword = () => {
+  //get password length
+  getPasswordLength();
+  //if char type selected - what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
+  //if lowercase selected
+  //if uppercase selected
+  // if numeric selected
+  // if special chars selected
+
+
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
