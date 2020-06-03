4f04acf72718fe4eb0c5989f92ba21b1d64219ae
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-02 21:12:46 -0700

    pseudocode added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+/generates password based on user prompt inputs
+const generatePassword = () => {
+  //get password length btwn 8-128 characters
+  //if char type selected - what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
+  //if lowercase selected
+  //if uppercase selected
+  // if numeric selected
+  // if special chars selected
+  //return password based on selections
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
