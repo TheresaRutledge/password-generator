1bcd9d7db030cbf85c7e16ef5433a6956496657b
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-02 21:45:02 -0700

    better laid out pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
 // Assignment code here
 
-/generates password based on user prompt inputs
+//generates password based on user prompt inputs
 const generatePassword = () => {
-  //get password length btwn 8-128 characters
-  //if char type selected - what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
-  //if lowercase selected
-  //if uppercase selected
-  // if numeric selected
-  // if special chars selected
-  //return password based on selections
+  //function to randomly select a char types. Parameter is array of types available
+  //function to randomly select character based on charater type
+  //prompt password length btwn 8-128 characters
+  //prompt what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
+  //take chosen char types into an array
+  //create password array and fill first indexes with character types selected to ensure each has at least one representation
+  //pass types array to random type generator for remainder of password charaters 
+  //once type is generated, randomly assign a character within type to password array
+  //once password array has reach length goal convert to string
+  //return password string
+
+
+  //Functions:
+  
+//random charater type generator
+
+//random lowercase generator
+
+//random uppercase generator
+
+//random numeric generator
+
+//random special character generator
+
+//prompt password length function
+
+//prompt character types selection
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
