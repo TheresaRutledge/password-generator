b4f378e5b6832c0c0c1b6f3cd854ec9524c6f545
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-04 14:03:01 -0700

    prompts for character type criteria requiring at least one selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
- //function to randomly select a char types. Parameter is array of types available
-  //function to randomly select character based on charater type
-  //prompt password length btwn 8-128 characters
-  //prompt what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
-  //take chosen char types into an array
-  //create password array and fill first indexes with character types selected to ensure each has at least one representation
-  //pass types array to random type generator for remainder of password charaters 
-  //once type is generated, randomly assign a character within type to password array
-  //once password array has reach length goal convert to string
-  //return password string
+//function to randomly select a char types. Parameter is array of types available
+//function to randomly select character based on charater type
+//prompt password length btwn 8-128 characters
+//prompt what char types should be included (min one selected): lowercase, uppercase,numeric,special chars
+//take chosen char types into an array
+//create password array and fill first indexes with character types selected to ensure each has at least one representation
+//pass types array to random type generator for remainder of password charaters 
+//once type is generated, randomly assign a character within type to password array
+//once password array has reach length goal convert to string
+//return password string
 
-  //Functions:
-  
+//Functions:
+
 //random charater type generator
 
 //random lowercase generator
@@ -23,15 +23,30 @@
 
 //prompt user for character types selection. Must choose at least one
 
+const getCharacterTypes = () => {
+  alert('Select which character types to inlcude. Must select at least one');
+  var lowercase = window.confirm('Include lowercase?');
+  var uppercase = window.confirm('Include uppercase?');
+  var numeric = window.confirm('Include numeric?');
+  var specialCharacters = window.confirm('Include special characters?');
+  if (
+    !lowercase && !uppercase && !numeric && !specialCharacters
+  ){
+    alert('Must select at least one character type.')
+    getCharacterTypes();
+  }
+}
+
+
 
 //function to prompt user for password length between 8-128
 const getPasswordLength = () => {
   let passwordLength = prompt('Choose password length between 8 and 128 characters.');
   passwordLength = parseInt(passwordLength);
-  if (!passwordLength){
+  if (!passwordLength) {
     alert('Length must be a number');
     getPasswordLength();
-  } else if(passwordLength<8 || passwordLength > 128){
+  } else if (passwordLength < 8 || passwordLength > 128) {
     alert('Password must be between 8 and 128 characters.');
     getPasswordLength();
   }
@@ -43,8 +58,10 @@ const getPasswordLength = () => {
 
 //generates password based on user prompt inputs
 const generatePassword = () => {
-  //password length
-let length = getPasswordLength();
+  //get password length
+  let length = getPasswordLength();
+  //get character types
+  let charTypes = getCharacterTypes();
 }
 
 
